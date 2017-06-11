e65ee706c5090cd281d1dddda3e87e7ffb0db3a8
MauricioAcosta <andres.taamap@gmail.com>
2017-06-10 23:06:11 -0500

    Clases y decoradores

---
+++ clases_metodos/lampara.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+class LamparaDeEscritorio:
+
+    _LAMPS = ['''
+    lámpara encendida
+    ''',
+    '''
+    lámpara apagada
+''']
+
+    def __init__(self, is_turned_on):
+        self._is_turned_on = is_turned_on
+
+    def turn_on(self):
+        self._is_turned_on = True
+        self._display_image()
+
+    def turn_off(self):
+        self._is_turned_on = False
+        self._display_image()
+
+    def _display_image(self):
+
+        if self._is_turned_on:
+            print(self._LAMPS[0])
+        else:
+            print(self._LAMPS[1])
+
+def run():
+
+    lamp = LamparaDeEscritorio(is_turned_on=False)
+
+    while True:
+        command = str(raw_input('''
+        ¿Que deseas hacer?
+        [p]render
+        [a]pagar
+        [s]alir
+        '''))
+
+        if command == 'p':
+            lamp.turn_on()
+        elif command == 'a':
+            lamp.turn_off()
+        elif command == 's':
+            print('Hasta luego')
+            break
+        else:
+            print('comando incorrecto')
+
+
+if __name__ == '__main__':
+    run()
